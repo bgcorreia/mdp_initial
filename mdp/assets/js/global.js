543534a83ba43e21779bb76a1d5884089ce71808
Bruno Correia <brunogomescorreia@gmail.com>
2018-01-08 23:36:32 -0300

    Upload with ajax/json work

--- mdp/assets/js/global.js
+++ mdp/assets/js/global.js
@@ -1,8 +1,30 @@
-$('input[type="file"]').change(function(){
+$('body').on('submit', '#form', function(e){
+	e.preventDefault();
+	var formData = new FormData($(this)[0]);
+	$.ajax({
+		url : "./upload_form.php",
+		type: "POST",
+		processData: false,
+		contentType: false,
+		data: formData,
+		success: function(data){
+			if (data.error) {
+				console.info(data.error);
+			} else if (data.classes) {
+				$("#param").html("");
+				$.each(data.classes, function() {
+					$("#param").append("<option>"+this+"</option>");
+				});
+			}
+		}
+	});
+});
+$('input[type=file]').change(function(){
 	var a = $(this),
 		b = a.val(),
 		c = b.substr(b.lastIndexOf('\\') + 1);
 	a.next().next().html(c);
+	$('#form').submit();
 });
 var page = {
 	href: function(){
